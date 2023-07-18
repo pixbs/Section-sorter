2886ecf05ca10c3f801caf7c3e346e5426f5562c
pixbs <hey@pixbs.com>
2023-07-18 18:35:55 +0200

    Added new UI

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,34 +1,137 @@
-// This is a counter widget with buttons to increment and decrement the number.
-
 const { widget } = figma
-const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, useWidgetId} = widget
+const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, useWidgetId,Input} = widget
+
+
+const arrowLeftSrc = `
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M20.7071 16L20.3536 15.6465L17.3536 12.6465L16.6464 13.3536L18.7929 15.5L10.5 15.5L10.5 16.5L18.7929 16.5L16.6464 18.6465L17.3536 19.3536L20.3536 16.3536L20.7071 16Z" fill="black"/>
+</svg>
+`
+const arrowDownSrc = `
+<svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M17 20.7071L16.6465 20.3536L13.6465 17.3536L14.3536 16.6464L16.5 18.7929L16.5 10.5H17.5L17.5 18.7929L19.6465 16.6464L20.3536 17.3536L17.3536 20.3536L17 20.7071Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+const refreshSrc = `
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M22.6094 13.6877L21.4347 15.156C21.4266 15.1039 21.4177 15.0519 21.4081 15C21.2566 14.1806 20.92 13.4019 20.4187 12.7254C19.7167 11.778 18.7287 11.0813 17.6005 10.7381C16.4723 10.3949 15.2638 10.4234 14.153 10.8193C13.3599 11.1021 12.6467 11.5614 12.0645 12.1576C11.8314 12.3964 11.6193 12.6571 11.4315 12.9372L11.4317 12.9373L12.2621 13.4941L12.2623 13.4942C12.7995 12.6928 13.5801 12.0854 14.4889 11.7614C15.3976 11.4375 16.3864 11.4142 17.3094 11.695C18.2324 11.9757 19.0408 12.5457 19.6152 13.3209C20.0999 13.975 20.3964 14.746 20.4771 15.5506L18.2772 14.084L17.7225 14.916L20.7225 16.916L21.1039 17.1703L21.3902 16.8123L23.3902 14.3123L22.6094 13.6877ZM10.6094 15.1877L8.60938 17.6877L9.39024 18.3123L10.5649 16.844C10.573 16.8961 10.5819 16.9481 10.5915 17C10.743 17.8194 11.0796 18.5981 11.5809 19.2746C12.283 20.222 13.271 20.9187 14.3991 21.2619C15.5273 21.6051 16.7359 21.5766 17.8466 21.1807C18.6397 20.8979 19.3529 20.4386 19.9351 19.8424C20.1677 19.6041 20.3794 19.344 20.5669 19.0646L20.5681 19.0628L20.5679 19.0627L19.7375 18.5059L19.7373 18.5058L19.7363 18.5074C19.1991 19.308 18.4189 19.9148 17.5108 20.2386C16.602 20.5625 15.6132 20.5858 14.6902 20.305C13.7672 20.0243 12.9589 19.4543 12.3845 18.6791C11.8997 18.025 11.6032 17.254 11.5225 16.4494L13.7225 17.916L14.2772 17.084L11.2772 15.084L10.8957 14.8297L10.6094 15.1877Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+const verticalAlignSrc = `
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20.5 22.5L20.5 20.5L10.5 20.5L10.5 22.5L9.5 22.5L9.5 19.5L21.5 19.5L21.5 22.5L20.5 22.5ZM21.5 12.5L9.5 12.5L9.5 9.5L10.5 9.5L10.5 11.5L20.5 11.5L20.5 9.5L21.5 9.5L21.5 12.5ZM18.5 16.5L18.5 15.5L12.5 15.5L12.5 16.5L18.5 16.5Z" fill="black"/>
+</svg>
+`
+
+const sectionSrc = `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.86667 3H3V6.66667M8.86667 3H14V14H3V6.66667M8.86667 3V6.66667H3" stroke="black" stroke-linejoin="round"/>
+</svg>
+`
+const frameSrc = `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M6 2.5V3V5H10V3V2.5H11V3V5H13H13.5V6H13H11L11 10H13H13.5V11H13H11V13V13.5H10V13V11H6V13V13.5H5V13V11H3H2.5V10H3H5L5 6H3H2.5V5H3H5V3V2.5H6ZM10 10V6H6L6 10H10Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+const groupSrc = `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M9 3H7V4H9V3ZM11.5 12H12V11.5H13V12V13H12H11.5V12ZM4 7V9H3V7H4ZM12 4.5V4H11.5V3H12H13V4V4.5H12ZM12 7V9H13V7H12ZM4 4.5V4H4.5V3H4H3V4V4.5H4ZM3 12V11.5H4V12H4.5V13H4H3V12ZM9 12H7V13H9V12Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+const componentSrc = `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M5.74254 4.74819L7.99981 2.50024L10.2571 4.74819L7.99981 6.99614L5.74254 4.74819ZM4.74795 10.2573L2.5 8.00004L4.74795 5.74278L6.9959 8.00004L4.74795 10.2573ZM10.2571 11.2519L7.9998 13.4999L5.74253 11.2519L7.9998 9.00396L10.2571 11.2519ZM13.4996 8.00006L11.2517 5.74279L9.00371 8.00006L11.2517 10.2573L13.4996 8.00006Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+const instanceSrc = `
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M1.82812 8L2.164 7.66412L7.664 2.16412L7.99988 1.82825L8.33575 2.16412L13.8358 7.66412L14.1716 8L13.8358 8.33587L8.33575 13.8359L7.99988 14.1717L7.664 13.8359L2.164 8.33587L1.82812 8ZM7.99988 12.8282L12.8281 8L7.99988 3.17175L3.17163 8L7.99988 12.8282Z" fill="black" fill-opacity="0.8"/>
+</svg>
+`
+
+let nudge = "16"
 
 function Widget() {
-  const widgetId = useWidgetId()
-  
+  const [text, setText] = useSyncedState("text", "Hello\nWidgets");
+
   return (
-    <AutoLayout
-      verticalAlignItems={'center'}
-      spacing={8}
-      padding={16}
-      cornerRadius={8}
-      fill={'#FFFFFF'}
-      stroke={'#E6E6E6'}
-      onClick={() => {
-        const widgetNode = figma.getNodeById(widgetId) as WidgetNode;
-        const widgetParent = widgetNode.parent
-        if(!(widgetParent?.type === "SECTION")){
-          return
-        }
-        const children = widgetParent.findChildren(node => node.type === "FRAME"|| node.type === "SECTION") as SceneNode[]
-        Sort(children, 16)
-      }}
+    <AutoLayout name="body"
+    direction="vertical"
+    fill="#fff"
+    cornerRadius={8} // let's do in future corner radius as parent corner radious
+    padding={16}
+    spacing={12}
     >
-      <SVG
-        src={`<svg width="22" height="15" viewBox="0 0 22 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9026 1.43168C12.1936 1.47564 12.4822 1.54098 12.7663 1.62777L12.7719 1.62949C14.0176 2.0114 15.109 2.78567 15.8858 3.83854L15.8918 3.84665C16.5473 4.73808 16.9484 5.78867 17.058 6.88508L14.0863 4.88858L13.3259 6.02047L17.3852 8.74774L17.9079 9.09894L18.2994 8.60571L21.0056 5.19662L19.9376 4.34879L18.3531 6.34479C18.3424 6.27511 18.3306 6.20563 18.3179 6.13636C18.1135 5.02233 17.6601 3.96334 16.9851 3.04274L16.9791 3.03462C16.0303 1.74427 14.6956 0.794984 13.1714 0.326388L13.1658 0.32466C12.8171 0.217755 12.4627 0.137298 12.1055 0.0832198C10.899 -0.0994351 9.66061 0.0188515 8.50099 0.435448L8.4947 0.437711C7.42511 0.823053 6.46311 1.44778 5.6774 2.25801C5.38576 2.55876 5.11841 2.88506 4.87886 3.23416C4.85856 3.26376 4.83845 3.29351 4.81854 3.32343L5.94262 4.08294L5.94802 4.07484C5.96253 4.0531 5.97717 4.03146 5.99195 4.00993C6.71697 2.95331 7.75331 2.15199 8.95541 1.72013L8.9617 1.71788C9.33245 1.58514 9.71301 1.48966 10.098 1.43156C10.6957 1.34135 11.3039 1.34123 11.9026 1.43168ZM3.70034 6.39429L0.994141 9.80338L2.06217 10.6512L3.64663 8.65521C3.65741 8.72489 3.66916 8.79437 3.68187 8.86364C3.88627 9.97767 4.33964 11.0367 5.01467 11.9573L5.02063 11.9654C5.96945 13.2557 7.30418 14.205 8.82835 14.6736L8.83398 14.6753C9.18281 14.7823 9.53732 14.8628 9.89464 14.9168C11.101 15.0994 12.3393 14.9811 13.4988 14.5646L13.5051 14.5623C14.5747 14.1769 15.5367 13.5522 16.3224 12.742C16.614 12.4413 16.8813 12.115 17.1209 11.7659C17.1412 11.7363 17.1613 11.7065 17.1812 11.6766L16.0571 10.9171L16.0518 10.9252C16.0372 10.9469 16.0225 10.9686 16.0078 10.9902C15.2827 12.0467 14.2464 12.848 13.0444 13.2799L13.0381 13.2821C12.6673 13.4149 12.2868 13.5103 11.9018 13.5684C11.3041 13.6587 10.6958 13.6588 10.0971 13.5683C9.8062 13.5244 9.51754 13.459 9.23347 13.3722L9.22784 13.3705C7.98212 12.9886 6.89078 12.2143 6.11393 11.1615L6.10795 11.1534C5.45247 10.2619 5.05138 9.21133 4.94181 8.11492L7.91342 10.1114L8.6739 8.97953L4.61459 6.25226L4.09188 5.90106L3.70034 6.39429Z" fill="black"/>
-        </svg>`}
-      ></SVG>
+      <AutoLayout name="headline"
+      spacing={8}
+      width={"fill-parent"}
+      >
+        <Text name="headline-text"
+        fontSize={26}
+        fontWeight={700}
+        width={"fill-parent"}
+        >
+          Name of the section
+        </Text>
+        <AutoLayout name="direction"
+        cornerRadius={2}
+        stroke="#E6E6E6"
+        >
+          <SVG src={arrowLeftSrc} 
+            fill="#E6E6E6"
+          />
+          <SVG src={arrowDownSrc}/>
+        </AutoLayout>
+        <AutoLayout name="input-wrapper"
+        cornerRadius={2}
+        stroke="#E6E6E6"
+        >
+          <SVG name="direction-icon"
+          src={verticalAlignSrc}
+          />
+          <Input
+          onTextEditEnd={(e) => setText(e.characters)}
+          placeholder="edit me"
+          value={nudge}
+          width={48}
+          fontSize={11}
+          verticalAlignText="center"
+          height={"fill-parent"}
+          />
+        </AutoLayout>
+        <SVG name="refresh-button" 
+        src={refreshSrc}
+        cornerRadius={2}
+        stroke="#E6E6E6"
+        />
+      </AutoLayout>
+      <AutoLayout name="stats"
+      spacing={16}
+      width={"fill-parent"}
+      >
+        {Stat(sectionSrc,"Section",8)}
+        {Stat(frameSrc,"Frame",8)}
+        {Stat(groupSrc,"Group",8)}
+        {Stat(componentSrc,"Component",8)}
+        {Stat(instanceSrc,"Instance",8)}
+      </AutoLayout>
+    </AutoLayout>
+  )
+}
+
+function Stat(svgSrc: string, label : string, number : number){
+  if(number == 0){
+    return
+  }
+  return (
+    <AutoLayout name={label}
+    spacing={4}>
+      <SVG src={svgSrc}/>
+      <Text
+      fontSize={11}
+      >
+        {label}: {number}
+      </Text>
     </AutoLayout>
   )
 }
